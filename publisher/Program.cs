2709b74f1a448014b900833219ba9185c3870534
Aylin <aylinmzarate@gmail.com>
2024-09-26 14:49:41 -0500

    first commit

---
+++ publisher/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:29092" };
+var producer = new ProducerBuilder<Null, string>(config).Build();
+
+while (true) {
+    var book = Novels.Random();
+    var message = new Message<Null, string> { Value = JsonSerializer.Serialize(book) };
+    await producer.ProduceAsync("books", message);
+    Console.WriteLine($"Produced book '{book.Title}' by {book.Author} ({book.Year})");
+    await Task.Delay(1000);
+}
---
+++ readers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+
+var config = new ConsumerConfig { BootstrapServers = "localhost:29092", GroupId = "reader" };
+var consumer = new ConsumerBuilder<Null, string>(config).Build();
+consumer.Subscribe("books");
+Console.WriteLine("Subscribed to books.");
+
+while (true) {
+    var consumed = consumer.Consume();
+    var book = JsonSerializer.Deserialize<Book>(consumed.Message.Value)!;
+    Console.WriteLine($"Consumed book '{book.Title}' by {book.Author} ({book.Year})");
+}
